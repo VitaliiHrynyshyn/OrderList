d5437415aaa24c39752484cdb789214e9597f56d
Vitalii <vitaliy-ira@outlook.com>
2019-03-13 00:09:07 +0200

    fix

--- ListOrders/Models/OrderContext.cs
+++ ListOrders/Models/OrderContext.cs
@@ -12,7 +12,6 @@ namespace ListOrders.Models
         public OrderContext(DbContextOptions<OrderContext> options)
             : base(options)
         {
-            Database.EnsureCreated();
         }
     }
 }
